63b0b0f8423f0d00ca7cc3e537c2282c593364c5
ka’imi <kaimi@piratenfraktion-nrw.de>
2013-09-19 14:32:40 +0200

    timeline.js eingebunden (dirty)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    createStoryJS({
+            type: 'timeline',
+            width: '100%',
+            height: '600',
+            source: 'timeline.json',
+            lang: 'de'
+    });
+});
