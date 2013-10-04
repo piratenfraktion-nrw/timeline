d772dac705cb86fd46e0522a31c6529c35961276
ka’imi <kaimi@piratenfraktion-nrw.de>
2013-10-04 12:06:30 +0200

    release

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(function() {
     createStoryJS({
             type: 'timeline',
             width: '100%',
-            height: '600',
+            height: '800',
             source: 'timeline.json',
             lang: 'de',
         });
